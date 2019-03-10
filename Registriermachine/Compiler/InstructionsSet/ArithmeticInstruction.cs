11f1139450aef87ee7e84ce23bee369dc9e9890d
Florian Bucher <florian.bucher@bbv.ch>
2019-03-10 22:18:48 +0100

    arithmetic functions

---
+++ Registriermachine/Compiler/InstructionsSet/ArithmeticInstruction.cs
@@ -0,0 +1,38 @@
+﻿namespace Registriermachine.Compiler.InstructionsSet
+{
+    using System;
+    using Registriermachine.Register;
+
+    public class ArithmeticInstruction : IInstruction
+    {
+        public ArithmeticInstruction(
+            string registerResult,
+            string registerOperand1,
+            string registerOperand2,
+            Func<int, int, int> arithmeticFunction)
+        {
+            this.RegisterResult = registerResult;
+            this.RegisterOperand1 = registerOperand1;
+            this.RegisterOperand2 = registerOperand2;
+            this.ArithmeticFunction = arithmeticFunction;
+        }
+
+        public Func<int, int, int> ArithmeticFunction { get; }
+
+        public string RegisterOperand1 { get; }
+
+        public string RegisterOperand2 { get; }
+
+        public string RegisterResult { get; }
+
+        public void ExecuteOnRegister(IRegister register)
+        {
+            int valueOperand1 = register[this.RegisterOperand1];
+            int valueOperand2 = register[this.RegisterOperand2];
+
+            register[this.RegisterResult] = this.ArithmeticFunction.Invoke(valueOperand1, valueOperand2);
+
+            register[RegistriermachineConstants.ProgramCounterRegister] += 1;
+        }
+    }
+}
---
+++ Registriermachine/Compiler/InstructionsSet/ArithmeticInstructionTemplate.cs
@@ -0,0 +1,27 @@
+﻿namespace Registriermachine.Compiler.InstructionsSet
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class ArithmeticInstructionTemplate : BaseInstructionTemplate, IInstructionTemplate
+    {
+        private readonly Func<int, int, int> function;
+
+        public ArithmeticInstructionTemplate(string operationSymbol, Func<int, int, int> function)
+            : base(new Regex($@"^(?<registerResult>R\d+) \:\= (?<registerOperand1>R\d+) (?<operation>[{operationSymbol}]) (?<registerOperand2>R\d+)$"))
+        {
+            this.function = function;
+        }
+
+        public IInstruction Compile(string code)
+        {
+            Match match = this.CodePattern.Match(code);
+
+            string registerResult = match.Groups["registerResult"].Value;
+            string registerOperand1 = match.Groups["registerOperand1"].Value;
+            string registerOperand2 = match.Groups["registerOperand2"].Value;
+
+            return new ArithmeticInstruction(registerResult, registerOperand1, registerOperand2, this.function);
+        }
+    }
+}
